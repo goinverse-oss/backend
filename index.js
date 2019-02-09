30b9ff51b53f61c4227dbed89bdd06fc57c7cfe9
Brett Higgins <brett.higgins@gmail.com>
2019-02-09 17:41:29 -0500

    Implement Patreon Oauth2 process

--- index.js
+++ index.js
@@ -1,9 +1,53 @@
 const serverless = require('serverless-http');
+const bodyParser = require('body-parser');
 const express = require('express');
-const app = express();
+const qs = require('qs');
+const awsParamStore = require('aws-param-store');
+const axios = require('axios');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-})
+async function getPatreonClientCreds() {
+  const opts = { region: process.env.AWS_REGION };
+  const stage = process.env.SLS_STAGE;
+  const names = [
+    `/${stage}/PATREON_CLIENT_ID`,
+    `/${stage}/PATREON_CLIENT_SECRET`,
+  ];
+  const result = await awsParamStore.getParameters(names, opts);
+  const params = result.Parameters;
 
-module.exports.handler = serverless(app);
+  // eslint-disable-next-line camelcase
+  const [client_id, client_secret] = params.map(param => param.Value);
+  return { client_id, client_secret };
+}
+
+async function init() {
+  const app = express();
+
+  const creds = await getPatreonClientCreds();
+
+  app.use(bodyParser.urlencoded());
+  app.post('*/patreon/validate', async (req, res) => {
+    const url = 'https://www.patreon.com/api/oauth2/token';
+
+    const obj = {
+      ...req.body,
+      ...creds,
+    };
+    const body = qs.stringify(obj);
+    const patreonRes = await axios.post(url, body);
+    res.json(patreonRes.data);
+  });
+
+  return app;
+}
+
+let handler;
+
+module.exports.handler = async (event, context) => {
+  if (!handler) {
+    const app = await init();
+    handler = serverless(app);
+  }
+
+  return handler(event, context);
+};
