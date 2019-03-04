9b526144f56c9de46b7a9948ca5073e67e516470
Brett Higgins <brett.higgins@gmail.com>
2019-03-03 22:03:27 -0500

    Hook up Sentry error reporting; fix Patreon token error

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const qs = require('qs');
 const awsParamStore = require('aws-param-store');
 const axios = require('axios');
 const helmet = require('helmet');
+const morgan = require('morgan');
 const _ = require('lodash');
+const Sentry = require('@sentry/node');
 const { patreon: patreonAPI } = require('patreon');
 
 const stage = process.env.SLS_STAGE;
@@ -21,6 +23,9 @@ async function getCreds() {
       space: `/${stage}/CONTENTFUL_SPACE`,
       environment: `/${stage}/CONTENTFUL_ENVIRONMENT`,
       accessToken: `/${stage}/CONTENTFUL_ACCESS_TOKEN`,
+    },
+    sentry: {
+      dsn: `/${stage}/SENTRY_DSN`,
     },
   };
 
@@ -41,37 +46,35 @@ async function getCreds() {
 
 const CAMPAIGN_URL = 'https://www.patreon.com/bdhtest';
 
-async function filterData(contentfulData, patreonToken) {
-  let user = null;
+function canAccess(pledge, item, podcasts) {
+  const contentType = item.sys.contentType.sys.id;
+  if (contentType === 'podcastEpisode') {
+    const podcast = podcasts[item.fields.podcast.sys.id];
+    return (
+      _.get(podcast.fields, 'minimumPledgeDollars', null) === null
+        || (
+          !!pledge
+            && podcast.fields.minimumPledgeDollars * 100 <= pledge.amount_cents
+        )
+    );
+  }
+  if (contentType === 'meditation') {
+    return (
+      pledge && /Meditations/i.test(pledge.reward.title)
+    );
+  }
+  return true;
+}
+
+async function filterData(contentfulData, patreon) {
   let pledge = null;
 
-  if (patreonToken) {
-    const client = patreonAPI(patreonToken);
+  if (patreon.token) {
+    const client = patreonAPI(patreon.token);
     const { store, rawJson } = await client('/current_user?includes=pledges');
-    user = store.find('user', rawJson.data.id);
+    const user = store.find('user', rawJson.data.id);
 
     pledge = _.find(user.pledges, p => (p.reward.campaign.url === CAMPAIGN_URL));
-  }
-
-  function canAccess(item, podcasts) {
-    const contentType = item.sys.contentType.sys.id;
-    console.log(`contentType: ${contentType}`);
-    if (contentType === 'podcastEpisode') {
-      const podcast = podcasts[item.fields.podcast.sys.id];
-      return (
-        _.get(podcast.fields, 'minimumPledgeDollars', null) === null
-          || (
-            !!pledge
-              && podcast.fields.minimumPledgeDollars * 100 <= pledge.amount_cents
-          )
-      );
-    }
-    if (contentType === 'meditation') {
-      return (
-        pledge && /Meditations/i.test(pledge.reward.title)
-      );
-    }
-    return true;
   }
 
   // podcasts are included; pull them out by ID first
@@ -85,7 +88,7 @@ async function filterData(contentfulData, patreonToken) {
     ...contentfulData,
     items: contentfulData.items.map(
       (item) => {
-        if (canAccess(item, podcasts)) {
+        if (canAccess(pledge, item, podcasts)) {
           return _.set(item, 'fields.patronsOnly', false);
         }
 
@@ -104,51 +107,111 @@ async function filterData(contentfulData, patreonToken) {
   };
 }
 
+function wrapAsync(fn) {
+  // Async exceptions result in uncaught rejected promises,
+  // which bypass Express' error handling and crash the lambda.
+  // This ensures any uncaught exception gets passed to next()
+  // which lets Express handle it.
+  return (req, res, next) => (
+    fn(req, res, next).catch(next)
+  );
+}
+
+const patreonTokenUrl = 'https://www.patreon.com/api/oauth2/token';
+
 async function init() {
+  const { patreon, contentful, sentry } = await getCreds();
+
+  Sentry.init(sentry);
+  Sentry.configureScope((scope) => {
+    scope.setTag('stage', stage);
+  });
+
+  Sentry.addBreadcrumb({ message: 'Setting up Express app' });
+
   const app = express();
+  app.use(Sentry.Handlers.requestHandler());
+  app.use(Sentry.Handlers.errorHandler());
 
-  const { patreon, contentful } = await getCreds();
-
+  app.use(morgan('combined'));
   app.use(helmet());
 
   app.post(
     '*/patreon/validate',
     bodyParser.urlencoded({ extended: true }),
-    async (req, res) => {
-      const url = 'https://www.patreon.com/api/oauth2/token';
+    wrapAsync(
+      async (req, res) => {
+        const url = patreonTokenUrl;
 
-      const obj = {
-        ...req.body,
-        ...patreon,
-      };
-      const body = qs.stringify(obj);
-      const patreonRes = await axios.post(url, body, { validateStatus: null });
-      res.status(patreonRes.status).json(patreonRes.data);
-    },
+        const obj = {
+          ...req.body,
+          ...patreon,
+        };
+        const body = qs.stringify(obj);
+        const patreonRes = await axios.post(url, body, { validateStatus: null });
+        res.status(patreonRes.status).json(patreonRes.data);
+      },
+    ),
   );
 
-  app.get('*/contentful/*', async (req, res) => {
-    const path = req.path
-      .replace(new RegExp(`^(/${stage})?/contentful`), '')
-      .replace(/\/spaces\/[^/]+/, `/spaces/${contentful.space}`)
-      .replace(/\/environments\/[^/]+/, `/environments/${contentful.environment}`);
-    const host = 'https://cdn.contentful.com';
-    const url = `${host}/${path}`;
-    const contentfulRes = await axios.get(url, {
-      params: req.query,
-      validateStatus: null,
-      headers: {
-        authorization: `Bearer ${contentful.accessToken}`,
-      },
-    });
-    const patreonToken = _.get(req.headers, 'x-theliturgists-patreon-token');
-    const { status } = contentfulRes;
-    let { data } = contentfulRes;
-    if (status >= 200 && status < 300) {
-      data = await filterData(data, patreonToken);
-    }
-    res.status(status).json(data);
-  });
+  app.get('*/contentful/*', wrapAsync(
+    async (req, res) => {
+      Sentry.addBreadcrumb({ message: 'API request', data: req.path });
+      const path = req.path
+        .replace(new RegExp(`^(/${stage})?/contentful`), '')
+        .replace(/\/spaces\/[^/]+/, `/spaces/${contentful.space}`)
+        .replace(/\/environments\/[^/]+/, `/environments/${contentful.environment}`);
+      const host = 'https://cdn.contentful.com';
+      const url = `${host}/${path}`;
+      Sentry.addBreadcrumb({ message: 'Making contentful request', data: url });
+      const contentfulRes = await axios.get(url, {
+        params: req.query,
+        validateStatus: null,
+        headers: {
+          authorization: `Bearer ${contentful.accessToken}`,
+        },
+      });
+      const patreonAuth = {
+        token: _.get(req.headers, 'x-theliturgists-patreon-token'),
+      };
+      const { status } = contentfulRes;
+      let { data } = contentfulRes;
+      Sentry.addBreadcrumb({ message: 'Got contentful response', data });
+
+      let patreonError;
+      if (status >= 200 && status < 300) {
+        try {
+          data = await filterData(data, patreonAuth);
+        } catch (e) {
+          patreonError = e.error;
+        }
+      }
+      if (patreonError) {
+        res.status(patreonError.status).json({
+          error: (
+            'Error verifying Patreon status. '
+            + 'Please re-connect Patreon and try again.'
+          ),
+        });
+      } else {
+        res.status(status).json(data);
+      }
+    },
+  ));
+
+  app.use(
+    async (error, req, res, next) => {
+      console.error('Uncaught error:', error);
+
+      // force sentry to flush any buffered eents
+      Sentry.captureException(error);
+      await Sentry.getCurrentHub().getClient().close(2000);
+
+      res.status(500).json({
+        error: _.pick(error, ['message', 'stack']),
+      });
+    },
+  );
 
   return app;
 }
@@ -157,10 +220,21 @@ let handler;
 
 module.exports.init = init;
 module.exports.handler = async (event, context) => {
-  if (!handler) {
-    const app = await init();
-    handler = serverless(app);
+  try {
+    if (!handler) {
+      Sentry.addBreadcrumb({ message: 'Initializing handler' });
+      const app = await init();
+      handler = serverless(app);
+    }
+  } catch (err) {
+    console.error('Uncaught error in init:', err);
+    Sentry.captureException(err);
+
+    throw err;
   }
 
+  Sentry.addBreadcrumb({ message: 'Handling http event', data: event });
+
+  // does its own error handling
   return handler(event, context);
 };
