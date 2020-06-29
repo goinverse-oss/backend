0cea1119230534ffd4e9f6d011566d1e4d974edf
Brett Higgins <brett.higgins@gmail.com>
2020-06-28 23:26:04 -0400

    Remove unused test data

--- src/pledge.test.js
+++ src/pledge.test.js
@@ -1,106 +1,16 @@
 const { Pledge } = require('./pledge');
 
-const patreonUserData = {
-  "data": {
-    "attributes": {},
-    "id": "user-1",
-    "relationships": {
-      "pledges": {
-        "data": [
-          {
-            "id": "pledge-1",
-            "type": "pledge"
-          }
-        ]
-      }
-    },
-    "type": "user"
-  },
-  "included": [
-    {
-      "attributes": {
-        "amount_cents": 2500,
-      },
-      "id": "pledge-1",
-      "relationships": {
-        "reward": {
-          "data": {
-            "id": "reward-1",
-            "type": "reward"
-          }
-        }
-      },
-      "type": "pledge"
-    },
-    {
-      "attributes": {
-        "amount": 2500,
-        "amount_cents": 2500,
-        "published": true,
-        "published_at": "2020-05-20T21:43:11.382+00:00",
-        "title": "Tier Name",
-        "unpublished_at": null
-      },
-      "id": "reward-1",
-      "type": "reward"
-    },
-  ]
-};
-
-const tier = {
-  "fields": {
-    "title": "Tier Tame",
-    "description": "Tier Description",
-    "amountCents": 2500,
-    "isUnpublished": false,
-    "isDeleted": false,
-    "patreonId": "349058",
-    "canAccessMeditations": true,
-    "canAccessLiturgies": true,
-    "adFreeListening": true,
-    "podcasts": [
-      {
-        "sys": {
-          "type": "Link",
-          "linkType": "Entry",
-          "id": "podcast1"
-        }
-      },
-      {
-        "sys": {
-          "type": "Link",
-          "linkType": "Entry",
-          "id": "podcast2"
-        }
-      },
-      {
-        "sys": {
-          "type": "Link",
-          "linkType": "Entry",
-          "id": "podcast3"
-        }
-      }
-    ],
-    "patronsOnly": false
-  }
-};
-
-const podcast = {
-  "sys": {
-    "id": "podcast2",
-    "type": "Entry",
-    "createdAt": "2019-02-26T17:48:04.562Z",
-    "updatedAt": "2020-06-16T03:26:31.230Z"
-  },
-  "fields": {
-    "title": "Super special patron podcast",
-    "description": "Text text text",
-    "patronsOnly": true
-  }
-};
 
 describe('Pledge', () => {
   it('denies access when tier is null', () => {
+    const podcast = {
+      "sys": {
+        "id": "podcast2",
+      },
+      "fields": {
+        "patronsOnly": true
+      }
+    };
     const pledge = new Pledge(null, null);
     expect(pledge.isPatron()).toBe(false);
     expect(pledge.canAccessPodcast(podcast)).toBe(false);
